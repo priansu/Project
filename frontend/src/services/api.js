fa110c085066e082c3dc214abfbaab55a6fac357
Vidya20044pps <vidya2004pps@gmail.com>
2026-05-01 11:39:30 +0530

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://genuine-connection-production-df0f.up.railway.app/api",
 });
 
 export default API;
