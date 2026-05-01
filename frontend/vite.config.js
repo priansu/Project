736688133c3d02cef1996f9c6db21fd15bf7f6ff
Vidya20044pps <vidya2004pps@gmail.com>
2026-05-01 11:49:54 +0530

    final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    host: '0.0.0.0'
+  },
+
   preview: {
     host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: [
-      'fortunate-empathy-production-ac69.up.railway.app'
-    ]
+    allowedHosts: true
   }
 })
